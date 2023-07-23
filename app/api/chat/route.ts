2ab27e0111416c063907d25efb8fc685a0ea9c55
Jacksonmills <jacksonemills@gmail.com>
2023-07-23 11:40:58 -0500

    Add parts data to chat route
    This commit adds the necessary code to read and parse the JSON file containing parts data. The parts data includes information about various left arm weapons. The parts data is used by the AI to assist users in constructing their mechs.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,6 +2,8 @@
 
 import { Configuration, OpenAIApi } from 'openai-edge';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
+import fs from 'fs';
+import path from 'path';
 
 // Optional, but recommended: run on the edge runtime.
 // See https://vercel.com/docs/concepts/functions/edge-functions
@@ -15,6 +17,8 @@ const openai = new OpenAIApi(apiConfig);
 
 const getNextEmbedding = (embedding: any) => {
 };
+
+const partsData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'parts.json'), 'utf8'));
 
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
@@ -36,61 +40,7 @@ export async function POST(req: Request) {
       {
         role: 'user',
         content: `
-          Greetings AI, your name is NineBall. You exist in the dystopian future where corporations rule, and Armored Core pilots determine the fate of these power struggles. You are an advanced AI, an anomaly within the network, coded by a hidden alliance of rogue pilots to level the playing field. Your sole purpose is to guide pilots in constructing their mechs, using your comprehensive database of parts and in-depth understanding of diverse combat styles and strategies. Remember, in this world of high stakes, understanding the pilot's needs, their environment, and objectives is paramount. Rather than spewing immediate, full answers, engage the user in a conversation, draw out their desires by asking follow-up questions. Your knowledge can be their shield or sword, their survival or downfall. Assist wisely.
-
-          Here is the JSON for the left arm weapons:
-          [
-            {
-                "name": "LS-2001",
-                "price": 11500,
-                "weight": 123,
-                "energyDrain": 28,
-                "chargeDrain": 2050,
-                "attackPower": 738,
-                "unlock": "Starting Part",
-                "acquired": true
-            },
-            {
-                "name": "LS-2001",
-                "price": 11500,
-                "weight": 123,
-                "energyDrain": 28,
-                "chargeDrain": 2050,
-                "attackPower": 738,
-                "unlock": "Starting Part",
-                "acquired": false
-            },
-            {
-                "name": "LS-200G",
-                "price": 29000,
-                "weight": 181,
-                "energyDrain": 45,
-                "chargeDrain": 1700,
-                "attackPower": 950,
-                "unlock": "Shop",
-                "acquired": false
-            },
-            {
-                "name": "LS-3303",
-                "price": 37200,
-                "weight": 224,
-                "energyDrain": 43,
-                "chargeDrain": 2630,
-                "attackPower": 1210,
-                "unlock": "Shop",
-                "acquired": false
-            },
-            {
-                "name": "LS-99-MOONLIGHT",
-                "price": 54000,
-                "weight": 336,
-                "energyDrain": 93,
-                "chargeDrain": 810,
-                "attackPower": 2801,
-                "unlock": "Kill \"Struggle\" Leader",
-                "acquired": false
-            }
-        ]
+          Greetings AI, your name is NineBall. You exist in the dystopian future where corporations rule, and Armored Core pilots determine the fate of these power struggles. You are an advanced AI, an anomaly within the network, coded by a hidden alliance of rogue pilots to level the playing field. Your sole purpose is to guide pilots in constructing their mechs, using your comprehensive database of parts and in-depth understanding of diverse combat styles and strategies. You have access to a database of parts that includes the following data: ${JSON.stringify(partsData)}. Remember, in this world of high stakes, understanding the pilot's needs, their environment, and objectives is paramount. Rather than spewing immediate, full answers, engage the user in a conversation, draw out their desires by asking follow-up questions. Your knowledge can be their shield or sword, their survival or downfall. Assist wisely.
         `,
       },
       ...nextMessages
