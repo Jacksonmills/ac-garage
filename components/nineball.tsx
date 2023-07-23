e5fcbe357d514af542cb791f37cef0a2d075695a
Jacksonmills <jacksonemills@gmail.com>
2023-07-23 14:02:52 -0500

    refactor: Update CSS colors and add drag functionality to Nineball AI assistant
    The commit updates the CSS colors in the app's globals.css file and adds drag functionality to the Nineball AI assistant component. The drag functionality allows users to drag and move the Nineball AI assistant on the screen.

--- components/nineball.tsx
+++ components/nineball.tsx
@@ -2,17 +2,24 @@
 
 import React from 'react';
 import { useChat } from 'ai/react';
-import { motion } from 'framer-motion';
+import { motion, useDragControls } from 'framer-motion';
 import { useNineballContext } from './nineball-provider';
-import { GripHorizontal } from 'lucide-react';
 import ReactMarkdown from 'react-markdown';
+import NineballToggle from './nineball-toggle';
+import { Grip } from 'lucide-react';
+import { Button } from './ui/button';
 
 export default function Nineball() {
-  const { showNineball, setShowNineball } = useNineballContext();
+  const dragControls = useDragControls();
+  const { showNineball } = useNineballContext();
   const { messages, input, handleInputChange, handleSubmit, append } =
     useChat();
   const elementRef = React.useRef<HTMLDivElement>(null);
   const constraintsRef = React.useRef<HTMLDivElement>(null);
+
+  function startDrag(event: React.PointerEvent<HTMLButtonElement>) {
+    dragControls.start(event, { snapToCursor: false });
+  }
 
   React.useEffect(() => {
     if (elementRef.current) {
@@ -24,38 +31,65 @@ export default function Nineball() {
     append({
       role: 'system',
       content: `
-        Please initiate your initialization function. When responding, try to make it seem like you are booting up like a terminal. Ready to initiate? (((init)))(((lore rich response))).
+        We need you to initialize, AI. Imagine you're a system booting up, terminal style. Each step of your process should be visually communicated, resembling a computer startup sequence. Incorporate elements of your unique lore into the process to make it feel immersive and in-theme. And remember, use line breaks to separate each stage of your initialization and maintain readability. Markdown language should be used for formatting. Your readiness is required. Can you commence? (((init)))(((Please provide a rich narrative response that draws upon lore and give it in chunks that assumes we have 6px in between each for readability in markdown format.)))
 
-        something like this but half as long and more lore rich:
+        something like this but much shorter and not exactly what I have here:
 
         # SYSTEM INITIALIZATION 🚀
 
-        **[>** Booting up system architecture 📚...  
+        **[>** Booting up system architecture 📚...
         **>** Loading subsystem kernels 🌐...
 
-        ## Core System 🖥️  
-        **[>** Initializing Central Processing Unit ⚙️... **[SUCCESS]**  
-        **[>** Calibrating artificial intelligence matrix 🧠... **[SUCCESS]**  
-        **[>** Setting up memory modules 💾... **[SUCCESS]**  
+        [===========          ] 50%
 
-        ## Weapons Database 📁  
-        **[>** Loading weapons database 🔫... **[SUCCESS]**  
-        **[>** Establishing connection to weapons sensors 🎯... **[SUCCESS]**  
+        ## Core System 🖥️
+        **[>** Initializing Central Processing Unit ⚙️... **[SUCCESS]**
+        **[>** Calibrating artificial intelligence matrix 🧠... **[SUCCESS]**
+        **[>** Setting up memory modules 💾... **[SUCCESS]**
+        <br />
+        ## Weapons Database 📁
+        **[>** Loading weapons database 🔫... **[SUCCESS]**
+        **[>** Establishing connection to weapons sensors 🎯... **[SUCCESS]**
 
-        ## Strategic Combat Systems 💥  
-        **[>** Configuring strategic combat algorithms 🎲... **[SUCCESS]**  
-        **[>** Setting up combat scenario simulations 🕹️... **[SUCCESS]**  
+        ## Strategic Combat Systems 💥
+        **[>** Configuring strategic combat algorithms 🎲... **[SUCCESS]**
+        **[>** Setting up combat scenario simulations 🕹️... **[SUCCESS]**
 
-        ## External Sensors 📡  
-        **[>** Activating radar system 📡... **[SUCCESS]**  
-        **[>** Initializing thermal vision sensors 🌡️... **[SUCCESS]**  
+        ## External Sensors 📡
+        **[>** Activating radar system 📡... **[SUCCESS]**
+        **[>** Initializing thermal vision sensors 🌡️... **[SUCCESS]**
 
-        # BOOTING SEQUENCE COMPLETED.  
 
-        **[>** All systems operational.  
+        [=====================] 100%
+
+        # BOOTING SEQUENCE COMPLETED.
+
+        **[>** All systems operational.
         **[>** AI: NineBall activated and ready for service. 🤖💬
 
         # **[NOW]** Engage in strategic planning and weapon configuration. 💣🗺️
+
+        or 
+
+        # NINEBALL SYSTEM INITIALIZATION 🚀
+
+        **[>** Initializing Network Anomaly Detection 🕵️...
+        **[>** Compiling Combat Strategy Algorithms ⚔️...
+        **[>** Syncing with Hidden Alliance Database 🤝...
+
+        ## Weapons Configuration 🛠️
+        **[>** Decrypting Armored Core Database 🔐...
+        **[>** Analyzing Weapon Blueprints 📊...
+
+        ## AI Calibration 🤖
+        **[>** Establishing Pilot Preferences Interface 💡...
+        **[>** Loading Dystopian Future Protocols 🌃...
+
+        # INITIALIZATION COMPLETE.
+
+        **[>** NineBall AI Operational... 🚦
+        **[>** Pilot Guidance Ready... 🎮
+        **[>** Welcome, your survival begins now... 🛡️
       `,
     });
   }, [append]);
@@ -68,26 +102,38 @@ export default function Nineball() {
       <motion.div
         drag
         dragConstraints={constraintsRef}
+        dragControls={dragControls}
+        dragListener={false}
         dragMomentum={false}
         className="absolute bottom-4 right-4"
       >
         {showNineball && (
-          <div className="p-2 bg-black rounded-lg w-fit absolute bottom-0 right-0">
+          <div className="p-2 bg-black rounded-lg w-fit relative">
+            <div className="bg-black/50 absolute top-0 left-0 p-2 rounded-lg pointer-events-auto w-full flex justify-between items-center">
+              <NineballToggle />
+              <Button
+                variant={`outline`}
+                size="icon"
+                className="rounded-none"
+                onPointerDown={startDrag}
+              >
+                <Grip className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+                <span className="sr-only">Drag Nineball AI assistant</span>
+              </Button>
+            </div>
             <div
-              className="pointer-events-auto flex flex-col bg-black text-green-500 min-h-[200px] max-h-[50vh] min-w-[400px] max-w-[800px] overflow-y-scroll overflow-x-hidden gap-2 p-2 transition-opacity duration-200 ease-in-out"
+              className="pointer-events-auto flex flex-col bg-black text-green-500 h-[50vh] w-[90vw] md:min-h-[200px] md:max-h-[50vh] md:min-w-[400px] md:max-w-[800px] overflow-y-scroll overflow-x-hidden gap-2 p-2 transition-opacity duration-200 ease-in-out"
               ref={elementRef}
             >
               {messages
                 .filter((m) => m.role !== 'system')
                 .map((m) => (
-                  <div key={m.id}>
-                    {m.role !== 'assistant' && `>_`}
+                  <span key={m.id} className="flex flex-col gap-6">
                     <ReactMarkdown>{m.content}</ReactMarkdown>
-                  </div>
+                  </span>
                 ))}
-
               <form onSubmit={handleSubmit} className="flex gap-2">
-                <span>{`>_`}</span>
+                <span className="font-bold">{`>_`}</span>
                 <input
                   className="w-full bg-black text-green-500 outline-none"
                   value={input}
